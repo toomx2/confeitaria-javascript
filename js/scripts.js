48c4259dbb0e5454cfae9c18f47a6f52ebea4348
THOMAS SANTOS DE SOUZA <thomas.ssouza1@senacsp.edu.br>
2026-03-13 11:59:04 -0300

    atualizando

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,35 @@ btnResetar.addEventListener('click', () => {
     botao.style.display = 'inline-block';
     btnResetar.style.display = 'none';
 });
+
+
+/* Card Bolo Maracujá */
+
+const botaoCalc = document.getElementById('btn-calcular1');
+const btnReset = document.getElementById('btn-resetar1');
+const recibo = document.getElementById('voucher');
+
+// Botão Gerar
+
+botaoCalc.addEventListener('click', () => {
+    const precoOriginal = 105;
+    const porcentagem = 15;
+
+    const valorDesconto = (precoOriginal * porcentagem) / 100;
+    const precoFinal = precoOriginal - valorDesconto;
+
+    document.getElementById('valor-desc1').innerText = valorDesconto.toFixed(2);
+    document.getElementById('total-final1').innerText = `R$ ${precoFinal.toFixed(2)}`;
+
+    botaoCalc.style.display = 'none';
+    btnReset.style.display = 'inline-block';
+    recibo.style.display = 'block';
+});
+
+// Botão Resetar
+
+btnReset.addEventListener('click', () => {
+    recibo.style.display = 'none';
+    botaoCalc.style.display = 'inline-block';
+    btnReset.style.display = 'none';
+});
